b8eaf462eca3c295ab4e1568cbd9e844e5ad41fa
Sanjay-P-21 <psanjaygowda@gmail.com>
2024-10-02 01:40:12 +0530

    recent

--- ATB_JavaPrograms/src/tasks_ATBjava/task_of_250924_03.java
+++ ATB_JavaPrograms/src/tasks_ATBjava/task_of_250924_03.java
@@ -3,7 +3,21 @@ package tasks_ATBjava;
 public class task_of_250924_03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Factorial Program - n = 5 , Output = 120 //  n = 5 ->  Fact = 5*4*3*2*1 = 120
+
+        // n = 1 ->  Fact = 1*1 -> 1
+
+        // n = 3 -> Fact = 3*2*1 -> 6 
+		int n = 6;
+		
+		long factorial = 1;
+		
+		for (int i=1; i<=n;i++) {
+			factorial *=i;}
+				
+			System.out.println(factorial);
+			System.out.println(4*3*2*1);
+		
 
 	}
 
