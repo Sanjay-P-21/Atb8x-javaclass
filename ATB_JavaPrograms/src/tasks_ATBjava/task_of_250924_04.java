b9828ecbfa196001d7654b69af8e669132e24c11
Sanjay-P-21 <psanjaygowda@gmail.com>
2024-10-02 14:00:44 +0530

    recent

--- ATB_JavaPrograms/src/tasks_ATBjava/task_of_250924_04.java
+++ ATB_JavaPrograms/src/tasks_ATBjava/task_of_250924_04.java
@@ -3,7 +3,7 @@ package tasks_ATBjava;
 public class task_of_250924_04 {
 
 	public static void main(String[] args) {
-		// TO print the vowels in the alphabet using for loop
+		// TO print the vowels in the alphabet using for loop//
 		
 		String vowels = "AEIOUaeiou";
 		
